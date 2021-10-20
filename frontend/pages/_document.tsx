bae6066ffa5592a0f6b3fb0c0ead537a3f987d9b
Kevin Cam <kevinsebcam@gmail.com>
2021-10-20 11:03:36 -0400

    Fix type errors during build.

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,17 +1,20 @@
-import Document, { DocumentContext } from 'next/document';
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 import { ComponentClass, PropsWithChildren } from 'react';
 import { AppInitialProps } from 'next/app';
+import { AppType, Enhancer } from 'next/dist/shared/lib/utils';
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp:
-            (App: ComponentClass) =>
-            (props: PropsWithChildren<AppInitialProps>) =>
+            (App: Enhancer<AppType>) =>
+            (props: PropsWithChildren<DocumentInitialProps>) =>
               sheet.collectStyles(<App {...props} />),
         });
       const initialProps = await Document.getInitialProps(ctx);
