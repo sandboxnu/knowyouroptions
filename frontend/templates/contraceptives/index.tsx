4fe887f7cae11adb8afdd75a6e7bbaaefb785a45
Kevin Cam <kevinsebcam@gmail.com>
2021-10-26 12:58:13 -0400

    Create structure for contraceptives + methods templates

---
+++ frontend/templates/contraceptives/index.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from 'react';
+
+
+const Contraceptives = () => {
+  // states:
+  return (
+    <>
+      <div>
+        <h1></h1>
+        <>
+        </>
+      </div>
+    </>
+  );
+}
