c0ae090c38a6d80f5d618904f3d973027b316952
SwimmerDill <dillonscott.2024@gmail.com>
2021-10-28 18:55:33 -0400

    Contraceptive Tab Bar

---
+++ frontend/templates/TabBar.tsx
@@ -0,0 +1,48 @@
+import { ReactElement } from 'react';
+import styled from 'styled-components';
+
+const HighlightedTab = styled.div`
+  border-bottom: 0.25rem #5f034c;
+  font-weight: bold;
+  margin: 0 1rem;
+  white-space: nowrap;
+`;
+
+const Row = styled.div`
+  box-shadow: 0rem 0.75rem 1rem -0.5rem lightgrey;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  overflow-y: hidden;
+  padding: 1rem;
+  wrap: no-wrap;
+`;
+
+const Tab = styled.div`
+  margin: 0 1rem;
+  opacity: 0.2;
+  white-space: nowrap;
+`;
+
+const TabBar = ({
+  tabs,
+  tabIndex,
+  setTabIndex,
+}: {
+  tabs: Array<string>;
+  tabIndex: number;
+  setTabIndex: Function;
+}): ReactElement => {
+  return (
+    <Row>
+      {tabs.map((tab: string, index: number) => {
+        const highlightedTab = <HighlightedTab>{tab}</HighlightedTab>;
+        const regularTab = <Tab onClick={() => setTabIndex(index)}>{tab}</Tab>;
+        return index === tabIndex ? highlightedTab : regularTab;
+      })}
+    </Row>
+  );
+};
+
+export default TabBar;
--- frontend/templates/contraceptives/index.tsx
+++ frontend/templates/contraceptives/index.tsx
@@ -1,15 +1,26 @@
-import React, { ReactElement } from 'react';
-import Overview, { OverviewProps } from './tabs/Overview'
+import React, { ReactElement, useState } from 'react';
+import Overview, { OverviewProps } from './tabs/Overview';
+import TabBar from '../TabBar';
 
 export declare interface ContraceptivesProps {
-  title: string,
-  overviewProps: OverviewProps,
-};
+  title: string;
+  overviewProps: OverviewProps;
+}
 
 const Contraceptives = ({
   title,
   overviewProps,
 }: ContraceptivesProps): ReactElement => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const tabs = [
+    'Overview',
+    'Use',
+    'Efficacy',
+    'Effect',
+    'Mechanism',
+    'Practical Questions',
+    'Additional',
+  ];
   // states:
   // @ts-ignore
   return (
@@ -17,6 +28,7 @@ const Contraceptives = ({
       <div>
         <h1>{title}</h1>
       </div>
+      <TabBar tabs={tabs} tabIndex={tabIndex} setTabIndex={setTabIndex} />
       <div>
         <Overview {...overviewProps} />
       </div>
