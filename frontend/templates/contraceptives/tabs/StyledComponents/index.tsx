4d8216500451bc8ef3b46da84228450355ea4ca0
Kevin Cam <kevinsebcam@gmail.com>
2021-11-04 15:15:54 -0400

    Create standard styling for common tags.

---
+++ frontend/templates/contraceptives/tabs/StyledComponents/index.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Description = styled.p`
+  color: #7C7C7C;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export {
+  Column,
+  Description,
+  Row
+};
