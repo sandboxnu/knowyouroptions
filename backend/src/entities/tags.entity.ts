e83192c9546706dfc57c638b5bef8216518e02aa
aislinblack <1of4black@gmail.com>
2021-11-06 16:31:30 -0400

    Add in Contraceptive Types

---
+++ backend/src/entities/tags.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Tag {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  label: string;
+
+  @Column()
+  link: string;
+}
