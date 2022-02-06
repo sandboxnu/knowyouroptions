f3f199f8e33c367327c63369d489c232534dc7a5
Christina Long <long.c@husky.neu.edu>
2022-02-06 02:24:16 -0500

    added show/hide based off styled components

---
+++ frontend/pages/mediaSizes.tsx
@@ -0,0 +1,24 @@
+// from https://jsramblings.com/how-to-use-media-queries-with-styled-components/
+// thx Stefan <3 very cool
+import React, { ReactElement, useEffect, useState } from 'react';
+
+export const size = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px',
+};
+
+export const device = {
+  mobileS: `(min-width: ${size.mobileS})`,
+  mobileM: `(min-width: ${size.mobileM})`,
+  mobileL: `(min-width: ${size.mobileL})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(min-width: ${size.laptopL})`,
+  desktop: `(min-width: ${size.desktop})`,
+  desktopL: `(min-width: ${size.desktop})`,
+};
