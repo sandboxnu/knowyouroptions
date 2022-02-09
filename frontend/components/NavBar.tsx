4f9a94e0fa45ade6aaa8e0e1b0023dfd6a55348e
cheopsrule <blake.heyman@gmail.com>
2022-02-08 19:45:21 -0500

    Update navbar

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -1,116 +1,43 @@
-import { Button } from 'antd';
-import { ReactElement, useMemo, useState } from 'react';
-import {
-  MenuOutlined,
-  UserOutlined,
-  QuestionCircleOutlined,
-  BarChartOutlined,
-} from '@ant-design/icons';
+import { ReactElement } from 'react';
+import { UserOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
-import slideStyle from './slide.module.scss';
-import SvgBookmarkIcon from '../public/bookmark-nav-bar.svg';
-import SvgMenuIcon from '../public/menu.svg';
-import SvgQuickAccessButton from '../public/quick-access.svg';
-import SvgTakeQuestionnaire from '../public/take-questionnaire.svg';
+import SvgBookmarkIcon from '../public/BookmarkOutline.svg';
+import SvgProfileIcon from '../public/ProfileOutline.svg';
+import SvgSearchOutline from '../public/SearchOutline.svg';
+import SvgSettingsOutline from '../public/SettingsOutline.svg';
 
-const MenuHeading = styled.h1`
+const MenuHeading = styled.p`
   color: black;
-  font-family: 'din-2014';
-  font-size: 1rem;
-  padding-left: 15%;
-
-  display: inline-block;
-   {
-    margin-right: 10px;
-  }
+  font-family: 'roboto';
+  font-size: 16pt;
+  padding: 40px;
 `;
 
 const MenuElements = styled.div`
-  margin-left: 10%;
-  width: 75%;
-  :hover {
-    color: #911d7a;
-  }
-  & > div {
-    & > a {
-      color: #404040;
-      font-family: 'roboto';
-      font-size: 0.8rem;
-    }
-  }
-
-  > * {
-    margin-left: 15px;
-  }
-`;
-
-const MenuSection = styled.div`
-  border-bottom: 1.5px solid #d6d6d6;
-  padding: 1rem 0rem;
-  & > div {
-    display: flex;
-    flex-direction: column;
-    row-gap: 0.5rem;
-  }
-`;
-
-const TwoColumns = styled.div`
   display: flex;
-  margin-left: 10px;
-
-  > * {
-    margin-left: 20px;
-  }
-`;
-const ColumnItem = styled.div`
-  width: 50%;
-  :hover {
-    color: #800080;
-    font-weight: bold;
-  }
-  & > a {
-    font-family: 'roboto';
-    font-size: 0.8rem;
-  }
+  width: 80%;
+  justify-content: space-evenly;
 `;
 
 const Menu = styled.div`
-  border-left: none;
-  padding: 0rem 1rem;
-  height: 5v;
-  width: 100vw;
+  height: 135px;
   display: flex;
-  flex-direction: row;
+  flex-wrap: no-wrap;
+  justify-content: space-between;
+  align-items: center;
   background: white;
-  position: absolute;
-  z-index: 0;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 `;
 
 const MenuIcons = styled.div`
-  position: absolute;
-  right: 1%;
-  top: 0;
-  width: 25%;
+  display: flex;
 `;
 const Icon = styled.div`
-  padding-top: 10px;
-  padding-right: 10%;
-  display: inline-block;
-  float: right;
-  margin: auto;
+  padding: 1.5rem;
 `;
+
+// TODO: Switch between this sidebar and the one in Sidebar.tsx at a certain screen size.
 const Sidebar = (): ReactElement => {
-  const [open, setOpen] = useState<boolean>(true);
-  const [closing, setClosing] = useState<boolean>(false);
-
-  const closeMenu = () => {
-    setOpen(true);
-    setClosing(true);
-    return setTimeout(() => {
-      setClosing(false);
-    }, 500);
-  };
-
   return (
     <Menu>
       <MenuElements>
@@ -123,30 +50,21 @@ const Sidebar = (): ReactElement => {
 
       <MenuIcons>
         <Icon>
-          {' '}
-          <UserOutlined />
+          <SvgSearchOutline />
         </Icon>
 
         <Icon>
-          <UserOutlined />
+          <SvgBookmarkIcon />
         </Icon>
         <Icon>
-          <SvgBookmarkIcon></SvgBookmarkIcon>
+          <SvgSettingsOutline />
         </Icon>
 
         <Icon>
-          <SvgBookmarkIcon></SvgBookmarkIcon>
+          <SvgProfileIcon />
         </Icon>
       </MenuIcons>
     </Menu>
-  );
-};
-
-const Bookmark = (): ReactElement => {
-  return (
-    <>
-      <SvgBookmarkIcon />
-    </>
   );
 };
 
