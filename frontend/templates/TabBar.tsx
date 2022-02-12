79a281e4f02368d7cf786ca204a71f7f96d3eb05
allychao <chao.al@northeastern.edu>
2022-02-12 14:21:15 -0500

    fixed imports

--- frontend/templates/TabBar.tsx
+++ frontend/templates/TabBar.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from 'react';
 import styled from 'styled-components';
-import { device } from '../pages/mediaSizes';
+import { device } from './mediaSizes';
 
 const HighlightedTab = styled.h2`
   border: 0rem;
