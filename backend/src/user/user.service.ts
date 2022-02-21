04eb65915c426eb60b45f025bb09d3c194f77ae4
Stefan Philip <stefankphilip631@gmail.com>
2022-02-21 01:01:56 -0500

    docs for user service

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { User } from 'src/entities/user.entity';
 import { EmailIsTakenError } from 'src/error/email-taken-error';
 import { IncorrectPasswordError } from 'src/error/incorrect-password-error';
-import { NotLoggedInError } from 'src/error/not-logged-in-error';
 import { UnknownEmailError } from 'src/error/unknown-email-error';
 import { SignInInfo, UserInfo } from 'src/types/user';
 import { Connection } from 'typeorm';
@@ -13,6 +12,12 @@ export class UserService {
     private connection: Connection, // @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
+  /**
+   * Creates a user in the database with the given user information
+   * @param userInfo the information of the user to create
+   * @return the created User entity
+   * @throws EmailIsTakenError if the email is taken
+   */
   public async createUser(userInfo: UserInfo): Promise<User> {
     const existingUser = await User.findOne({
       email: userInfo.email,
@@ -31,6 +36,12 @@ export class UserService {
     }
   }
 
+  /**
+   * Returns the user associated with the specified sign-in info
+   * @param info sign in information
+   * @throws IncorrectPasswordError wrong password
+   * @throws UnknownEmailError email not found
+   */
   public async getUser(info: SignInInfo): Promise<User> {
     const existingUser = await User.findOne({
       email: info.email,
@@ -47,15 +58,15 @@ export class UserService {
     }
   }
 
+  /**
+   * Returns the user associated with the given ID
+   * @param id user ID
+   */
   public async getById(id: number): Promise<User> {
     const existingUser = await User.findOne({
       id: id,
     });
 
-    if (existingUser) {
-      return existingUser;
-    } else {
-      throw new NotLoggedInError();
-    }
+    return existingUser || undefined;
   }
 }
