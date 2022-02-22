cb32ce50b333894682d08aff81df6f545d31a80f
Stefan Philip <stefankphilip631@gmail.com>
2022-02-22 01:15:17 -0500

    oops need this for user repository

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from '../entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService],
   exports: [UserService],
 })
