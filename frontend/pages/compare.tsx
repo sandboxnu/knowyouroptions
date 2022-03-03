251840621da09e0023b9d089a22413fc3607b8e5
DilanRocco <captaindilan12@gmail.com>
2022-03-03 14:02:25 -0500

    compare page setup

---
+++ frontend/pages/compare.tsx
@@ -0,0 +1,29 @@
+import { ReactElement } from 'react';
+
+import SvgAcne from '../public/acne.svg';
+import SvgBed from '../public/bed.svg';
+import SvgBreastFeeding from '../public/breastfeeding.svg';
+import SvgBreastTenderness from '../public/breast-tenderness.svg';
+import SvgCalendar from '../public/calendar.svg';
+import SvgDepressed from '../public/depressed.svg';
+import SvgDoctor from '../public/doctor.svg';
+import SvgHeadache from '../public/headache.svg';
+import SvgImplant from '../public/implant.svg';
+import SvgImplantRemoval from '../public/implant-removal.svg';
+import SvgPad from '../public/pad.svg';
+import SvgTime from '../public/time.svg';
+
+import ContraceptiveTemplate from '../templates/contraceptives';
+import { EffectProps } from '../templates/contraceptives/tabs/Effect';
+import { EfficacyProps } from '../templates/contraceptives/tabs/Efficacy';
+import { OverviewProps } from '../templates/contraceptives/tabs/Overview';
+import { UseProps } from '../templates/contraceptives/tabs/Use';
+import { MechanismProps } from '../templates/contraceptives/tabs/Mechanism';
+import { PracticalProps } from '../templates/contraceptives/tabs/PracticalQuestions';
+import { AdditionalProps } from '../templates/contraceptives/tabs/AdditionalInfo';
+
+const Compare = (): ReactElement => {
+  return <div></div>;
+};
+
+export default Compare;
