fa4cccd65b08eb6c5c38b1a25bfde180c69a58d3
DilanRocco <captaindilan12@gmail.com>
2022-03-31 21:38:00 -0400

    working backend without bookmark get though

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,22 +1,39 @@
-import { Body, Controller, Get, Post, Param, Delete } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
-import { User } from 'src/entities/user.entity';
-import { UseGuards } from '@nestjs/common';
-//import { AppService } from './app.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CurrentUser } from '../decorators/currentuser.decorator';
+import { User } from '../entities/user.entity';
+
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
-
-  @Get(':id')
-  public getUser(@Param('id') id: number) {
-    return this.userService.getById(id);
+  constructor(private readonly UserService: UserService) {}
+  @Get()
+  getHello(): string {
+    return '';
   }
 
-  @Get()
+  /*
+    NOTES:
+    - JwtAuthGuard is an AuthGuard that restricts this route to only authenticated users
+        - Currently, this means that anyone making a GET request to /name/ has to have
+          a JWT auth_token in their cookies, attached to the request
+        - JwtAuthGuard takes the request, verifies the JWT in the cookies, and injects 
+          the current user into the Request  (see JwtStrategy)
+    - AuthGuards can be used at the route level, or the controller level
+    
+    - @CurrentUser() is a decorator that allows us to access the currently authenticated
+      user in a route that uses the JwtAuthGuard
+   */
+  /**
+   * Returns the name of the currently logged-in user
+   *
+   * @param user
+   */
+
+  @Get('name')
   @UseGuards(JwtAuthGuard)
-  public getBookmarks(@CurrentUser() user: User): string[] {
-    return ['dfsdf'];
+  async name(@CurrentUser() user: User): Promise<string> {
+    console.log('in app controller');
+    return user.name;
   }
 }
