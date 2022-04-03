b66ac7560c47143ba610e3771b1aab7a85e04fbe
cheopsrule <blake.heyman@gmail.com>
2022-04-03 14:32:56 -0400

    Redirect URL

--- backend/src/auth/google.strategy.ts
+++ backend/src/auth/google.strategy.ts
@@ -14,7 +14,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      callbackURL: 'http://localhost:3000/google/redirect',
+      callbackURL: 'http://localhost:3001/google/redirect',
       scope: ['email', 'profile'],
     });
   }
