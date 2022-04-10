e79d152dfaf74493a8a9c8039f0a270a2ef6b565
Stefan Philip <stefankphilip631@gmail.com>
2022-04-10 15:56:05 -0400

    font fix

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -35,6 +35,12 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="stylesheet" href="https://use.typekit.net/ejt2fks.css" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
